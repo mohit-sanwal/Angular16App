27b6e4b197baa299e3805486ccf96f5d4447df03
mohitSanwal <mohit123ongraph@gmail.com>
2023-07-21 18:15:25 +0530

    property binding, string interpolation, event binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import {FormsModule} from '@angular/Forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import {ErrorComponent} from './error/error.component';
+import {SuccessComponent} from './success/success.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    SuccessComponent,
+    ErrorComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // selector: '['app-servers']'
+  // selector: '.app-servers'
   selector: 'app-servers',
-  // templateUrl: './servers.component.html',
+  templateUrl: './servers.component.html',
   // for writting inline templates
-  template: `<app-server></app-server>`,
+  // template: `<app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+   isServer: boolean = true
+   constructor() {
+     setTimeout(() => {
+      this.isServer = false;
+     }, 2000)
+   }
+   
+    ngInit() {
 
+    }
+
+    handleApply() {
+      console.log('applied..')
+    }
 }
