d54dd29c24c20bb4fa85d3099f0cc0c40a36e45a
mohitSanwal <mohit123ongraph@gmail.com>
2023-07-21 18:13:08 +0530

    success and error toast implementation

---
+++ src/app/success/success.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'app-success',
+    templateUrl: './success.component.html',
+    styles: [`
+    .alert-success {
+        border: 1px solid green;
+        padding: 12px;
+    }
+    `]
+})
+
+export class SuccessComponent {
+
+}
