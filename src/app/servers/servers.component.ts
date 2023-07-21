1b8be70480bab2210e8d76447451daf9303427eb
mohitSanwal <mohit123ongraph@gmail.com>
2023-07-21 12:59:04 +0530

    inline templating

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // templateUrl: './servers.component.html',
+  // for writting inline templates
+  template: `<app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
