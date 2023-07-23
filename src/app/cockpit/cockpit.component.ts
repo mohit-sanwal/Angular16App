1057aa64a597ced249de5fe0c94fe6f2f36c9caa
mohitSanwal <mohit123ongraph@gmail.com>
2023-07-23 13:27:42 +0530

    viewChild implementation for localReferce

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,15 +6,63 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./cockpit.component.css', '../app.component.css']
 })
 export class CockpitComponent implements OnInit{
-  // bprintCreated is an alias for emit event
-  @Output() serverCreated = new EventEmitter<{name: string, content: string, type: string}>();
-  @Output('bprintCreated') bluePrintCreated = new EventEmitter<{name: string, content: string, type: string}>();
   newServerName = ''
   newServerContent =''
   serverElements = [];
-  onAddServer(nameInput: HTMLInputElement , contentInput: HTMLInputElement) {
-    this.newServerName = nameInput.value;
-    this.newServerContent = contentInput.value;
+  // bprintCreated is an alias for emit event
+  @Output() serverCreated = new EventEmitter<{name: string, content: string, type: string}>();
+  @Output('bprintCreated') bluePrintCreated = new EventEmitter<{name: string, content: string, type: string}>();
+  
+  @ViewChild('serverNameInput') serverNameInput: ElementRef ;
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
+
+
+  constructor() {
+
+  }
+  
+
+
+  
+  // onAddServer(nameInput: HTMLInputElement , contentInput: HTMLInputElement) {
+  //   this.newServerName = nameInput.value;
+  //   this.newServerContent = contentInput.value;
+  //   if(this.newServerName && this.newServerContent) {
+  //   this.serverCreated.emit({name: this.newServerName, content: this.newServerContent, type: 'server'});
+  //   this.newServerName = ''
+  //   this.newServerContent =''
+  //   // this.serverElements.push({
+  //   //   type: 'server',
+  //   //   name: this.newServerName,
+  //   //   content: this.newServerContent
+  //   // });
+  //   } else {
+  //     alert('please enter all the field before submit')
+  //   }
+  // }
+
+  // onAddBlueprint(nameInput: HTMLInputElement, contentInput: HTMLInputElement) {
+  //   this.newServerName = nameInput.value;
+  //   this.newServerContent = contentInput.value;
+  //   if(this.newServerName && this.newServerContent) {
+  //     this.bluePrintCreated.emit({name: this.newServerName, content: this.newServerContent, type: 'blueprint'})
+  //     // this.serverElements.push({
+  //     //   type: 'blueprint',
+  //     //   name: this.newServerName,
+  //     //   content: this.newServerContent
+  //     // });
+  //     this.newServerName = ''
+  //     this.newServerContent =''
+  //   } else {
+  //     alert('please enter all the field before submit')
+  //   }
+   
+  // }
+
+
+  onAddServer() {
+    this.newServerName = this.serverNameInput.nativeElement.value
+    this.newServerContent = this.serverContentInput.nativeElement.value
     if(this.newServerName && this.newServerContent) {
     this.serverCreated.emit({name: this.newServerName, content: this.newServerContent, type: 'server'});
     this.newServerName = ''
@@ -29,9 +77,9 @@ export class CockpitComponent implements OnInit{
     }
   }
 
-  onAddBlueprint(nameInput: HTMLInputElement, contentInput: HTMLInputElement) {
-    this.newServerName = nameInput.value;
-    this.newServerContent = contentInput.value;
+  onAddBlueprint() {
+    this.newServerName = this.serverNameInput.nativeElement.value
+    this.newServerContent = this.serverContentInput.nativeElement.value
     if(this.newServerName && this.newServerContent) {
       this.bluePrintCreated.emit({name: this.newServerName, content: this.newServerContent, type: 'blueprint'})
       // this.serverElements.push({
